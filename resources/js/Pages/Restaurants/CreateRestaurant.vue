81deb240d6695f9d4fd5a7194ceab8bce3028c09
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-22 12:30:14 +0200

    feat add create component- to finish

---
+++ resources/js/Pages/Restaurants/CreateRestaurant.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const newRestauant = useForm({
+    name: null,
+    address: null,
+    p_iva: null,
+    image: null,
+    description: null,
+});
+
+
+</script>
+
+<template>
+       
+  
+
+</template>
+
+
+
+<style></style>
