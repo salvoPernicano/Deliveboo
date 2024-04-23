d9a8a1c38016bceacff2ba584429240f3333dfa4
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-23 12:03:03 +0200

    feat relation restaurant_typology

--- database/migrations/2024_04_23_094118_create_typologies_table.php
+++ database/migrations/2024_04_23_094118_create_typologies_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('typologies', function (Blueprint $table) {
             $table->id();
+            $table->string('typology_name');
             $table->timestamps();
         });
     }
