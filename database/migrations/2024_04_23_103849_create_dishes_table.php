964179930e84a89dca7c68991f4ea9894f3c7e68
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-23 12:42:40 +0200

    migration dishes correction

--- database/migrations/2024_04_23_103849_create_dishes_table.php
+++ database/migrations/2024_04_23_103849_create_dishes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
-            $table->name('name', 250)->unique();
+            $table->string('name', 250)->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 4, 2);
             $table->boolean('visible')->default(true);
