5cc331a84f9f20a453163a46010f633991abe5f7
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-25 01:41:44 +0200

    fix graphics register form

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -43,81 +43,86 @@ const handleImageChange = (event) => {
 
 <template>
 
-        <div class="flex flex-col gap-9 justify-center items-center">
-            <h1 class="text-black text-5xl font-bold">Crea un nuovo post</h1>
+        <div class="flex flex-col  gap-9 justify-center items-center">
+            <h1 class="text-black text-5xl font-bold">Registra il tuo ristorante</h1>
 
             <!-- Unico form per entrambi i form -->
-            <form class="flex flex-col text-black text-lg text-center gap-7" @submit.prevent="handleSubmit" enctype="multipart/form-data">
-
+            <form class="bg-orange-400 w-2/3 max-w-3xl p-5 mb-10 rounded-lg flex flex-col  text-black text-lg text-center gap-7" @submit.prevent="handleSubmit" enctype="multipart/form-data">
+                <span class="italic text-start text-sm">I campi contrassegnati con * sono obbligatori</span>
                 <!-- Campi per il nuovo ristorante -->
-                <div class="flex flex-col">
-                    <label for="restaurantName">Inserisci nome Ristorante</label>
-                    <input id="restaurantName" class="text-black" type="text" name="name" v-model="registrationForm.restaurant_name">
+                <div class="flex items-center justify-center mx-auto w-4/5 s gap-4">
+                    <div class="w-full">
+                        <label class="font-bold" for="name">Nome utente *</label>
+                        <input id="name" type="text" class="mt-1 block w-full rounded-lg" v-model="registrationForm.name" required autofocus autocomplete="name">
+                        <InputError class="mt-2" :message="registrationForm.errors.name" />
+                    </div>
+                    <div class="w-full">
+                        <label class="font-bold" for="email">Email *</label>
+                        <input id="email" type="email" class="mt-1 block w-full rounded-lg" v-model="registrationForm.email" required autocomplete="username">
+                        <InputError class="mt-2" :message="registrationForm.errors.email" />
+                    </div>
+                </div>
+
+
+
+                <div class="flex items-center justify-center mx-auto w-4/5 s gap-4">
+                    <div class="w-full">
+                        <label class="font-bold" for="password">Password *</label>
+                        <input id="password" type="password" class="mt-1 block w-full rounded-lg" v-model="registrationForm.password" required autocomplete="new-password">
+                        <InputError class="mt-2" :message="registrationForm.errors.password" />
+                    </div>
+                    <div class="w-full">
+                        <label class="font-bold" for="password_confirmation">Conferma Password *</label>
+                        <input id="password_confirmation" type="password" class="mt-1 block w-full rounded-lg" v-model="registrationForm.password_confirmation" required autocomplete="new-password">
+                        <InputError class="mt-2" :message="registrationForm.errors.password_confirmation" />
+
+                    </div>
+                </div>
+
+    
+                <div class="flex flex-col w-4/5 mx-auto">
+                    <label class="font-bold" for="restaurantName">Inserisci nome Ristorante *</label>
+                    <input id="restaurantName" class="text-black rounded-md" type="text" name="name" v-model="registrationForm.restaurant_name" required>
                     <div v-if="errors.name"><span class="text-red-700">{{ errors.name }}</span></div>
                 </div>
 
-                <div class="flex flex-col">
-                    <label for="restaurantAddress">Inserisci indirizzo</label>
-                    <input id="restaurantAddress" class="text-black" type="text" name="address" v-model="registrationForm.address">
+                <div class="flex flex-col w-4/5 mx-auto">
+                    <label class="font-bold" for="restaurantAddress">Inserisci indirizzo *</label>
+                    <input id="restaurantAddress" class="text-black rounded-md" type="text" name="address" v-model="registrationForm.address" required>
                     <div v-if="errors.address"><span class="text-red-700">{{ errors.address }}</span></div>
                 </div>
 
-                <div class="flex flex-col">
-                    <label for="restaurantPIVA">Inserisci Partita Iva</label>
-                    <input id="restaurantPIVA" class="text-black" type="text" name="p_iva" v-model="registrationForm.p_iva">
+                <div class="flex flex-col w-4/5 mx-auto">
+                    <label class="font-bold" for="restaurantPIVA">Inserisci Partita Iva *</label>
+                    <input id="restaurantPIVA" class="text-black rounded-md" type="text" name="p_iva" v-model="registrationForm.p_iva" required>
                     <div v-if="errors.p_iva"><span class="text-red-700">{{ errors.p_iva }}</span></div>
                 </div>
 
-                <div class="flex flex-col">
-                    <label for="restaurantImage">Carica immagine</label>
-                    <input id="restaurantImage" type="file" name="image" accept="image/*" @change="handleImageChange">
+                <div class="flex flex-col w-4/5 bg-white rounded-lg p-2 border border-gray-500  mx-auto">
+                    <label class="font-bold" for="restaurantImage">Carica immagine</label>
+                    <input id="restaurantImage" class="mx-auto" type="file" name="image" accept="image/*" @change="handleImageChange">
                     <div v-if="errors.image"><span class="text-red-700">{{ errors.image }}</span></div>
                 </div>
 
-                <div class="flex flex-col">
-                    <label for="restaurantDescription">Inserisci descrizione</label>
-                    <textarea id="restaurantDescription" class="text-black" name="description" cols="30" rows="10" v-model="registrationForm.description"></textarea>
+                <div class="flex flex-col  w-4/5 mx-auto">
+                    <label class="font-bold" for="restaurantDescription">Inserisci una descrizione del ristorante</label>
+                    <textarea id="restaurantDescription" class="text-black rounded-lg" name="description" cols="30" rows="3" v-model="registrationForm.description"></textarea>
                     <div v-if="errors.description"><span class="text-red-700">{{ errors.description }}</span></div>
                 </div>
 
-                <div class="flex flex-col">
-                    <label for="restaurantTypologies">Seleziona tipologie di cucina</label>
-                    <select id="restaurantTypologies" v-model="registrationForm.selectedTypologies" multiple>
+                <div class="flex flex-col  mx-auto">
+                    <label class="font-bold" for="restaurantTypologies">Seleziona le tipologie di cucina del ristorante</label>
+                    <select id="restaurantTypologies" class="rounded-lg" v-model="registrationForm.selectedTypologies" multiple>
                         <option v-for="typology in typologies" :value="typology.id">{{ typology.typology_name }}</option>
                     </select>
                     <div v-if="errors.selectedTypologies"><span class="text-red-700">{{ errors.selectedTypologies }}</span></div>
                 </div>
 
                 <!-- Campi per la registrazione -->
-                <div>
-                    <label for="name">Name</label>
-                    <input id="name" type="text" class="mt-1 block w-full" v-model="registrationForm.name" required autofocus autocomplete="name">
-                    <InputError class="mt-2" :message="registrationForm.errors.name" />
-                </div>
 
-                <div class="mt-4">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="mt-1 block w-full" v-model="registrationForm.email" required autocomplete="username">
-                    <InputError class="mt-2" :message="registrationForm.errors.email" />
-                </div>
 
-                <div class="mt-4">
-                    <label for="password">Password</label>
-                    <input id="password" type="password" class="mt-1 block w-full" v-model="registrationForm.password" required autocomplete="new-password">
-                    <InputError class="mt-2" :message="registrationForm.errors.password" />
-                </div>
-
-                <div class="mt-4">
-                    <label for="password_confirmation">Confirm Password</label>
-                    <input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="registrationForm.password_confirmation" required autocomplete="new-password">
-                    <InputError class="mt-2" :message="registrationForm.errors.password_confirmation" />
-                </div>
-
-                <div class="flex items-center justify-end mt-4">
-                    <!-- <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Already registered?
-                    </Link> -->
-                    <button type="submit" class="bg-blue-950 hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded-full uppercase">registrti</button>
+                <div class="flex items-center justify-center gap-5 mt-4">
+                    <button type="submit" class="bg-white hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded-full uppercase">registati</button>
                 </div>
 
        
