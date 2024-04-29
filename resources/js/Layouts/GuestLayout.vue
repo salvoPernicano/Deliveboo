f33766f12a65898b6b6e151cf55eece281ec0a6c
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-29 12:14:52 +0200

    some spacing and sizing

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -6,7 +6,7 @@ import { Link } from '@inertiajs/vue3';
 function headerColor() {
     const header = document.getElementById("color");
     const scrolledPixels = window.scrollY;
-    
+
     if (scrolledPixels >= 400) {
         header.style.backgroundColor = "#F98F00";
     } else if (scrolledPixels < 400) {
@@ -18,10 +18,10 @@ window.addEventListener("scroll", headerColor);
 
 // Footer menu
 const footerMenu = [
-    {name: "Chi siamo"},
-    {name: "Ristoranti"},
-    {name: "Lavora con noi"},
-    {name: "Altro"}
+    { name: "Chi siamo" },
+    { name: "Ristoranti" },
+    { name: "Lavora con noi" },
+    { name: "Altro" }
 ];
 </script>
 
@@ -29,31 +29,30 @@ const footerMenu = [
     <header id="color" class="flex sm:justify-start items-center pt-6 sm:pt-0 fixed top-0 w-full">
         <div class="mx-auto lg:w-10/12 flex items-center gap-10">
             <Link href="/" class="flex items-center">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-                DeliveBoo
+            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+            DeliveBoo
             </Link>
-            
+
             <Link href="/" class="text-white hover:text-black font-bold">Home</Link>
         </div>
     </header>
     <slot />
 
-    <footer>
+    <footer class="">
         <div class="lg:w-10/12 mx-auto py-10 text-white">
             <div class="flex-column md:flex gap-10 justify-between">
                 <figure>
                     <Link href="/" class="flex items-center">
                     <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
                     DeliveBoo
-                </Link>
+                    </Link>
                 </figure>
                 <div v-for="(item) in 3">
                     <h6>
                         Scopri DeliveBoo
                     </h6>
                     <ul>
-                        <li v-for="(item, index) in footerMenu" :key="index"
-                        class="my-2 hover:text-black">
+                        <li v-for="(item, index) in footerMenu" :key="index" class="my-2 hover:text-black">
                             {{ item.name }}
                         </li>
                     </ul>
@@ -68,7 +67,7 @@ const footerMenu = [
 </template>
 
 <style scoped>
-    footer {
-        background-color: #FF6900;
-    }
+footer {
+    background-color: #FF6900;
+}
 </style>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,26 +30,20 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
+    <GuestLayout class="">
+
         <Head title="Log in" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
+        <form @submit.prevent="submit" class="my-56 mb-36 w-1/3 mx-auto h-3/4">
+            <div class="">
                 <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
+                    autocomplete="username" />
 
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
@@ -57,14 +51,8 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                    autocomplete="current-password" />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
@@ -76,13 +64,10 @@ const submit = () => {
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
+            <div class="flex flex-col gap-2 items-center justify-center mt-4">
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Forgot your password?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
