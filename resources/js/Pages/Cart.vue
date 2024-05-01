f7f11e233651c9e220869fa556713e5df0328b5d
salvoPernicano <salvo.pernicano@gmail.com>
2024-05-01 20:44:25 +0200

    fix graphic in cart

--- resources/js/Pages/Cart.vue
+++ resources/js/Pages/Cart.vue
@@ -1,39 +1,44 @@
 <template>
     <GuestLayout>
-        <div id="cart" class="bg-rose-300 min-h-screen w-full mt-24">
-            <h4 class="mb-6">Carrello</h4>
-            <div class="shadow border rounded-lg w-4/5 mx-auto">
-                <ul class="w-full flex flex-col items-center py-6">
-                    <li v-for="item in cartList" :key="item.id" class="my-4 mx-4 flex w-full">
-                        <Link :href="`/remove-from-cart/${item.id}`" class="mx-4 bg-yellow-500">
-                            Rimuovi piatto
+        <section class="bg-orange-400 text-white text-lg font-semibold">
+        <div id="cart" class="min-h-screen w-full mt-24">
+            <h4 class="mb-6 text-center">Carrello</h4>
+            <div class="shadow border rounded-lg w-4/5 mx-auto text-center">
+                <ul class="w-full flex flex-col items-center justify-center py-6">
+                    <li v-for="item in cartList" :key="item.id" class="p-4 flex items-center justify-center w-full">
+                        <Link :href="`/remove-from-cart/${item.id}`" class="rounded-lg bg-yellow-500">
+                            Remove Dish
                         </Link>
-
-                        <button @click="decreaseQuantity(item.id)" class="mx-4 bg-red-500">
+                        <button @click="decreaseQuantity(item.id)" class="mx-4">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-440v-80h560v80H200Z"/></svg>
                         </button>
-                        <div class="flex flex-wrap items-center w-full">
-                            <img :src="'storage/'+item.image" alt="product_image" class="w-14 h-14">
+                        <div class="flex items-center w-full">
+                            <img :src="'storage/'+item.image" alt="product_image" class="w-18 h-20">
                             <p class="w-3/5">{{ item.name }}</p>
-                            <span class="w-1/5">{{ item.price }}</span>
-                           <input type="text" :name="`quantity-${item.id}`" :id="`quantity-${item.id}`" :value="item.quantity" @change="">
-                            <hr class="mt-4 w-full">
+                            <div>
+                                <span class="w-1/5">price per unit €{{ item.price }}</span>
+                               <input type="text" :name="`quantity-${item.id}`" :id="`quantity-${item.id}`" :value="item.quantity" @change="" class="text-black">
+
+                            </div>
                         </div>
-                        <button @click="increaseQuantity(item.id)" class="mx-4 bg-blue-600">
+                        <button @click="increaseQuantity(item.id)" class="mx-4 ">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
                         </button>
-                        <button class="bg-green-600" @click="updateQty(item.id)">Conferma quantità</button>
+                        <button class="bg-green-600 rounded-lg" @click="updateQty(item.id)">Confirm quantity</button>
                     </li>
                 </ul>
-                <input type="number" v-model="total">
+                <div>
+                    <span>Total price : €{{ total }} </span>
+                   
+                </div>
             </div>
             <div class="w-4/5 mx-auto mt-8 flex justify-between">
                 <div class="w-4/5 mx-auto mt-8 flex justify-between">
-                <!-- <span>Totale: {{ total }}</span> -->
                 <button @click="checkout" class="bg-green-500 text-white px-4 py-2 rounded-md">Ordina e paga</button>
                 </div>
         </div>
         </div>
+    </section>
     </GuestLayout>
 </template>
 
@@ -45,7 +50,7 @@ import { Inertia } from '@inertiajs/inertia';
 
 const props = defineProps({
     cartList: {
-        type: Array,
+        type: Object,
         required: false
     }
 });
@@ -91,6 +96,6 @@ const updateQty = (productId) => {
 
 </script>
 
-<style scoped>
+<style>
 
 </style>
