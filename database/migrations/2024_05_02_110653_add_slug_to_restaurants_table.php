49aee5e27f666c73f99215177dc0f4d69fbed0e6
jacopo-derrico <derricoj.design@gmail.com>
2024-05-02 13:13:39 +0200

    add after for column position

--- database/migrations/2024_05_02_110653_add_slug_to_restaurants_table.php
+++ database/migrations/2024_05_02_110653_add_slug_to_restaurants_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('restaurants', function (Blueprint $table) {
-            $table->string('slug');
+            $table->string('slug')->after('name');
         });
     }
 
