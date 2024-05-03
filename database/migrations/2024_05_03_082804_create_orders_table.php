bc192b18364c680f712f508f9ce891f24687bb6f
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-05-03 10:59:10 +0200

    add table orders

---
+++ database/migrations/2024_05_03_082804_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('cart_total_price', 4, 2);
+            $table->string('name', 250);
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('address',250)->unique();
+            $table->string('name_doorbell')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
