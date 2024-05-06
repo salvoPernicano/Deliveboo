6a3f4d1ac8e099cfdf71252a8c35dddf37c54dd0
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-05-06 17:34:46 +0200

    dishes page made

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class OrderController extends Controller
 {
@@ -12,7 +16,27 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $userId = Auth::id();
+
+        // Recupera i ristoranti associati all'utente autenticato
+        $restaurants = Restaurant::where('user_id', $userId)->get();
+
+        // Recupera gli ordini associati ai ristoranti dell'utente autenticato
+        $orders = DB::table('dish_order')
+            ->select('orders.*') // Seleziona tutti i campi dalla tabella orders
+            ->join('orders', 'dish_order.order_id', '=', 'orders.id') // Esegui il join con la tabella orders
+            ->whereIn('dish_order.dish_id', function ($query) use ($userId) {
+                $query->select('id')
+                    ->from('dishes')
+                    ->whereIn('restaurant_id', function ($innerQuery) use ($userId) {
+                        $innerQuery->select('id')
+                            ->from('restaurants')
+                            ->where('user_id', $userId);
+                    });
+            })
+            ->orderByDesc('orders.created_at')->groupBy('orders.id') // Ordina gli ordini per data di creazione, più recenti per primi
+            ->get();
+        return Inertia::render('Orders', ['orders' => $orders]);
     }
 
     /**
---
+++ resources/js/Pages/Orders.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { defineProps } from 'vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/inertia-vue3';
+import { Inertia } from '@inertiajs/inertia';
+
+const props = defineProps({
+
+    orders: Array
+});
+
+
+</script>
+
+<style></style>
+<template>
+
+    <Head title="Crea piatto"></Head>
+    <AuthenticatedLayout>
+
+        <div class="p-3">
+            <div class="p-3 rounded-lg  shadow max-h-2/3 overflow-scroll">
+                <h4 class="font-bold pb-3">Ordini</h4>
+                <!-- table lapTop orders -->
+                <div class="flex ">
+                    <table class="w-full">
+                        <thead>
+                            <tr class="flex px-1">
+                                <th class="flex-1 text-[14px] text-start">Numero Ordine</th>
+                                <th class="flex-1 text-[14px] text-start">Nome cliente</th>
+                                <th class="flex-1 text-[14px] text-start">Indirizzo</th>
+                                <th class="flex-1 text-[14px] text-start">Campanello</th>
+                                <th class="flex-1 text-[14px] text-start">Mail</th>
+                                <th class="flex-1 text-[14px] text-end">Telefono</th>
+
+                            </tr>
+
+
+                        </thead>
+                        <tbody>
+                            <tr v-for="order in props.orders" class="bg-gray-100 rounded-lg flex mt-2 px-1">
+                                <td class="flex flex-1 gap-2 text-[12px] text-start relative">
+                                    {{ order.id }}
+                                </td>
+                                <td class="flex-1 text-[14px] text-start">{{ order.name }}</td>
+                                <td class="flex-1 text-[14px] text-start">{{ order.address }}</td>
+                                <td class="flex-1 text-[14px] text-start">{{ order.name_doorbell }}</td>
+                                <td class="flex-1 text-[14px] text-start">{{ order.email }}</td>
+                                <td class="flex-1 text-[14px] text-end">{{ order.phone }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
