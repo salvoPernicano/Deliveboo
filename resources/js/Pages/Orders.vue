84082977aa8cf85c48d5d516d97adc01a6016958
federicoverbicaro <federico.verbicaro@icloud.com>
2024-05-08 16:40:46 +0200

    fix_Orders MarginTop

--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -61,7 +61,7 @@ const props = defineProps({
             </div>
 
             <!-- orders resposive -->
-            <div v-for="order in props.orders" class="  p-3 rounded-lg shadow bg-white sm:hidden ">
+            <div v-for="order in props.orders" class="mt-3  p-3 rounded-lg shadow bg-white sm:hidden ">
 
                 <div class="flex justify-between">
                     <div class="flex items-center">
